b87f2d77d9718f02d5b54ff9ec5f9a6b34044225
MahatVasudev <mahatvasudev9@gmail.com>
2025-12-21 15:25:59 +0530

    Added New argument parses for scripts

--- src/utils/argparser.py
+++ src/utils/argparser.py
@@ -5,6 +5,15 @@ def str2bool(v):
     return str(v).lower() in ("true", "1", "yes", "y")
 
 
+def make_datalist(v: str):
+    v_list = v.split(',')
+    v_list[0] = v_list[0].removeprefix("[").removeprefix("(").strip()
+    v_list[-1] = v_list[-1].removesuffix("]").removesuffix(")").strip()
+
+    return v_list
+
+
+# NOTE: For ML scripts
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", type=int)
 parser.add_argument("--filename")
@@ -13,3 +22,12 @@ parser.add_argument('--log-loss', type=str2bool)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--weight-decay', type=float, default=0.0005)
 parser.add_argument('--scheduler-patience', type=int, default=3)
+
+
+# NOTE: For data_scripts
+download_script_parser = argparse.ArgumentParser()
+download_script_parser.add_argument("--years", type=make_datalist)
+download_script_parser.add_argument("--mode", type=int, choices=[0, 1, 2])
+download_script_parser.add_argument("--batch_size", type=int)
+download_script_parser.add_argument("--bin_size", type=float, default=0.25)
+download_script_parser.add_argument("--time", type=str, default="1d")
