dcc3155d2017f50cf8393814fd55539052525fa9
MahatVasudev <mahatvasudev9@gmail.com>
2025-12-21 15:24:39 +0530

    Small changes on the model architecture

--- src/models/CONVLSTM_NEW.py
+++ src/models/CONVLSTM_NEW.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
-from .CONVLSTM import ConvLSTM, ConvLSTMCell, ConvLSTM_NLAYERS
-from .Encoder import GridEncoder_SpatioAttention
+from .CONVLSTM import ConvLSTM_NLAYERS
 from .Auto_Encoder import AutoEncoderFire
 
 
 class CONVLSTM_FIREMODEL(nn.Module):
-    def __init__(self, auto_encoder: AutoEncoderFire, seq_len=7, hidden_channels=256):
+    def __init__(self, auto_encoder: AutoEncoderFire, hidden_channels=256):
         super().__init__()
 
         self.auto_encoder = auto_encoder
@@ -14,15 +13,14 @@ class CONVLSTM_FIREMODEL(nn.Module):
         self.convlstm = ConvLSTM_NLAYERS(
             input_channel=hidden_channels,
             hidden_channels=[
+                hidden_channels//4,
                 hidden_channels//2,
-                hidden_channels,
-                hidden_channels*2
+                hidden_channels
             ],
             kernel_size=3,
-            seq_len=seq_len
         )
         self.latent_proj = nn.Conv2d(
-            in_channels=hidden_channels*2,
+            in_channels=hidden_channels,
             out_channels=hidden_channels,
             kernel_size=1
         )
